43023fb1dc192878718c1305b8aa27ffb2323eeb
Léo <leo.chaumont15@gmail.com>
2026-01-19 14:12:22 +0100

    feat(pricing): add config loader

--- src/main/java/com/devops/cicd/PricingConfigLoader.java
+++ src/main/java/com/devops/cicd/PricingConfigLoader.java
@@ -1,4 +1,30 @@
 package com.devops.cicd;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class PricingConfigLoader {
+
+    public PricingConfig load() {
+        Properties prop = new Properties();
+
+        // On charge le fichier depuis le classpath
+        try (InputStream input = PricingConfigLoader.class.getClassLoader().getResourceAsStream("app.properties")) {
+
+            if (input == null) {
+                System.out.println("Impossible de trouver le fichier app.properties");
+                return null;
+            }
+
+            prop.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return new PricingConfig(Double.valueOf(required(prop, "vatRate")), (Double.valueOf(required(prop, "freeShippingThreshold"))));
+    }
+
+    private String required(Properties props, String key) {
+        return props.getProperty(key);
+    }
 }
