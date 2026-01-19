4f221f4655ebb58ecd64bad768a7d15a7d4c02b7
Léo <leo.chaumont15@gmail.com>
2026-01-19 12:40:28 +0100

    fix(main): fix package path

--- src/main/java/com/devops/cicd/Main.java
+++ src/main/java/com/devops/cicd/Main.java
@@ -1,4 +1,4 @@
-package java.com.devops.cicd;
+package com.devops.cicd;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
