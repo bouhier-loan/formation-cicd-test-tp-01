71e5ee5673f3177925d38124677050b400db21fc
Léo <leo.chaumont15@gmail.com>
2026-01-19 14:31:22 +0100

    fix(password): fix is strong method

--- src/main/java/com/devops/cicd/PasswordPolicy.java
+++ src/main/java/com/devops/cicd/PasswordPolicy.java
@@ -3,7 +3,7 @@ package com.devops.cicd;
 public class PasswordPolicy {
 
     public static boolean isStrong(String password){
-        return password != null;
+        return password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$");
     }
 
 }
